f504ba90cb5928db1a4b8fea4c7494c2879cb8ea
huo1362120194 <huo1362120194@163.com>
2020-05-29 08:23:40 +0800

    2

--- src/main/java/com/spring/layer/controller/ComLayerController.java
+++ src/main/java/com/spring/layer/controller/ComLayerController.java
@@ -1,24 +1,15 @@
 package com.spring.layer.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.spring.layer.entity.Result;
 import com.spring.layer.entity.User;
 import com.spring.layer.service.UserService;
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class ComLayerController {
@@ -60,19 +51,5 @@ public class ComLayerController {
     public Result deleteUser(@PathVariable Integer id){
         int lines = userService.deleteUserById(id);
         return new Result(0,lines,null);
-    }
-
-    @Value("classpath:json/image.json")
-    private Resource resource;
-    @RequestMapping("/resource")
-    @ResponseBody
-    public JSONObject getResource(){
-        try {
-            String areaData =  IOUtils.toString(resource.getInputStream(), Charset.forName("UTF-8"));
-            return (JSONObject) JSON.parse(areaData);
-        } catch (IOException e) {
-            logger.error("{}",e.getMessage(),e);
-        }
-        return null;
     }
 }
